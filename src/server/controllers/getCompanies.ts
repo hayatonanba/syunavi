83509da14b3581b637b63b3474f3719160cfddc5
mii0829 <a5822057@ima.aim.aoyama.ac.jp>
2025-02-20 15:51:06 +0900

    変数名変更

--- src/server/controllers/getCompanies.ts
+++ src/server/controllers/getCompanies.ts
@@ -1,21 +1,17 @@
-// server/controllers/getCompanies.ts
-import { TypedHandler } from "@hono/zod-openapi";import { prisma } from "../../../prisma";
+import type { RouteConfigToHandler } from "@hono/zod-openapi";
+import { prisma } from "../../../prisma";
 import { getCompaniesRoute } from "../routes/appRoutes";
 
-export const getCompaniesHandler: TypedHandler<typeof getCompaniesRoute> = async (c) => {    const { id } = c.req.param();
-  const blogs = await prisma.blog.findMany({
+export const getCompaniesHandler: RouteConfigToHandler<typeof getCompaniesRoute> = async (c) => {
+  const companies = await prisma.company.findMany({
     include: {
-      company: {
+      user: {
         select: {
-            name: true,
-            state: true,
-            createdAt: true,
-            userId: true,
-            user: true
+          name: true,
+          image: true
         },
       },
     },
   });
-
-  return c.json(blogs, 200);
+  return c.json(companies, 200);
 };
--- src/server/routes/appRoutes.ts
+++ src/server/routes/appRoutes.ts
@@ -2,7 +2,7 @@ import { createRoute, z } from "@hono/zod-openapi";
 import { CompanyIdSchema, CompanySchema, CompaniesSchema, CreateCompanySchema } from "../models/appSchema";
 
 export const getCompaniesRoute = createRoute({
-  path: "/",
+  path: "/api/syunavi/",
   method: "get",
   description: "全企業の取得",
   responses: {
@@ -18,9 +18,9 @@ export const getCompaniesRoute = createRoute({
 })
 
 export const getCompanyByIdRoute = createRoute({
-  path: "/{id}",
+  path: "api/syunavi/{id}",
   method: "get",
-  description: "個別のブログ記事を取得",
+  description: "個別の企業情報を取得",
   request: {
     params: CompanyIdSchema
   },
@@ -30,7 +30,7 @@ export const getCompanyByIdRoute = createRoute({
       content: { "application/json": { schema: CompanySchema } }
     },
     404: {
-      description: "ブログが見つかりませんでした。",
+      description: "企業が見つかりませんでした。",
       content: {
         "application/json": {
           schema: z.null()
