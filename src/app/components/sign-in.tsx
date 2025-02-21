11f6712904790736fab2c0fbadc7e418afedafa5
hrt_ioo1279@outlook.jp <hrt_ioo1279@outlook.jp>
2025-02-21 17:45:05 +0900

    headerを作成，フォルダの切り分け

--- src/app/components/sign-in.tsx
+++ src/app/components/sign-in.tsx
@@ -1,14 +1,23 @@
-import { signIn } from "../../../auth"
- 
+import { signIn } from "../../../auth";
+import Image from "next/image";
+
 export default function SignIn() {
   return (
     <form
       action={async () => {
-        "use server"
-        await signIn("google",{redirectTo: "/myPage"})
+        "use server";
+        await signIn("google", { redirectTo: "/myPage" });
       }}
     >
-      <button type="submit">Signin with Google</button>
+      <button className="flex bg-[#F2F2F2] rounded-3xl px-2 py-1" type="submit">
+        <Image
+          src={"/google-logo.svg"}
+          alt="Google Logo"
+          width="24"
+          height="24"
+        />
+        <span className="text">Sign in with Google</span>
+      </button>
     </form>
-  )
-} 
+  );
+}
