b2423e6dea13050c0f4508f3ef4cd44d09a35bff
hir0 <katuyo1616.use@gmail.com>
2025-02-23 02:38:42 +0900

    feat: dbからデータ取得

--- src/app/(app)/myPage/page.tsx
+++ src/app/(app)/myPage/page.tsx
@@ -1,16 +1,32 @@
-import { userData } from "@/data/userData";
 import { Card, CardContent, CardHeader } from "@/src/components/ui/card";
 import { Building2, Clock } from "lucide-react";
 import { Calendar } from "lucide-react";
 import PostButton from "../../components/postbutton/postButton";
-export default function Page() {
-  const companyies = userData.companies;
-  console.log(companyies);
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export type UserData = {
+    id? : string
+    name? : string | null
+};
+
+export default async function Page() {
+  await createSenkou()
+  const userData = await getSenkou("uuid-4649")
+  const companies = userData.flows.map(flow => flow.companies).flat();
+  console.log(companies);
+  const session = await auth()
+  if(!session?.user){
+    redirect("/")
+  }
+  // console.log(session?.user)
+
   return (
     <div>
-       <div className="p-5"><PostButton /></div>
+      <div className="p-5"><PostButton userData={session?.user}/></div>
+      
       <div className="flex justyfy-between gap-5">    
-        {companyies.map((company) => (
+        {companies.map((company) => (
           <Card key={company.companyId} className="w-1/4">
             <CardHeader>
               <div className="flex justify-between items-center">
@@ -39,4 +55,51 @@ export default function Page() {
       </div>
     </div>
   );
+}
+
+async function createSenkou() {
+    const response = await fetch("https://8s6eohdua5.execute-api.us-east-1.amazonaws.com/prod/senkous", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: "uuid-4649",
+        userName: "potekiti",
+        flows: [
+          {
+            flowId: "uuid_hoge",
+            flowOrder: 1,
+            flowName: "一次面接",
+            companies: [
+              { companyId: "uuid_fuga", companyName: "IIJ" },
+              { companyId: "uuid_piyo", companyName: "さくらインターネット" }
+            ]
+          },
+          {
+            flowId: "uuid_hoge_fuga",
+            flowOrder: 2,
+            flowName: "最終面接",
+            companies: [
+              { companyId: "uuid_hoge_piyo", companyName: "NTTドコモ" }
+            ]
+          }
+        ]
+      })
+    });
+  
+    const data = await response.json();
+    console.log("Created:", data);
+}
+
+async function getSenkou(userId) {
+    const response = await fetch(`https://8s6eohdua5.execute-api.us-east-1.amazonaws.com/prod/senkous/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  
+    const data = await response.json();
+    return data;
 }
